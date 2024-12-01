ad0920d6789c75be7f42d8adf5cb9fdd183b81f9
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-12-01 21:00:56 +0800

    fix layout bug

--- app/src/compenents/Markdown/MDBlock.tsx
+++ app/src/compenents/Markdown/MDBlock.tsx
@@ -1,14 +1,15 @@
-import "./MDBlock.scss"
+import styles from "./MDBlock.module.scss";
+import "./MDBlock.scss";
 
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import 'katex/dist/katex.min.css';
-import remarkGfm from 'remark-gfm';
-import rehypeHighlight from 'rehype-highlight';
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
 // import 'highlight.js/styles/github.css'; // Use a highlight.js theme of your choice
-import 'highlight.js/styles/github.css'; // Hardcoded theme
+import "highlight.js/styles/github.css"; // Hardcoded theme
 
 interface Props {
   children: string;
@@ -22,7 +23,15 @@ export const MDBlock: React.FC<Props> = ({ children, className }) => {
       remarkPlugins={[remarkMath, remarkGfm]}
       rehypePlugins={[rehypeKatex, rehypeHighlight]}
       components={{
-        
+        pre: ({ children }) => {
+          return (
+            <>
+              <div className={styles["codeblock"]}>
+                <pre className={styles["pre"]}>{children}</pre>
+              </div>
+            </>
+          );
+        },
       }}
     >
       {children}
--- app/src/pages/books/TestBook/Chapter_1.tsx
+++ app/src/pages/books/TestBook/Chapter_1.tsx
@@ -30,11 +30,16 @@ export const Chapter_1: Page = {
 
 Let $X$ be a set and let $\\mathcal T$ be a topology on $X$; i.e., $(X, \\mathcal T)$ is a *topological space*. Let $x$ be an element of $X$.
 
+
 Nam vehicula dignissim $x \\in X$ enim sed bibendum \`this is code\`. Ut gravida lorem justo, non aliquam libero condimentum quis. Phasellus auctor nisl at magna euismod, eu pharetra est porttitor. Vivamus convallis orci vitae sollicitudin pretium. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam tempor
 
 ## The Axiom of Choice
 
 rhoncus mauris eu **gravida**. Nam faucibus lacus eget eleifend eleifend. *Nam sed posuere dolor*, vel dictum orci. Integer laoreet, massa ac consectetur volutpat, sem risus elementum leo, ut tincidunt nibh lectus in nunc. Vivamus non elit maximus ipsum euismod dignissim. Sed nibh ipsum, sodales sed metus id, sodales bibendum massa. Proin finibus nec nulla quis eleifend.
+
+$$
+\\int^a_b f(x) \\mathrm{d} x = \\sum_{i = 1}^\\infty (f(x) \\cdot \\Delta x).
+$$
 
 rhoncus mauris eu **gravida**. Nam faucibus lacus eget eleifend eleifend. *Nam sed posuere dolor*, vel dictum orci. Integer laoreet, massa ac consectetur volutpat, sem risus elementum leo, ut tincidunt nibh lectus in nunc. Vivamus non elit maximus ipsum euismod dignissim. Sed nibh ipsum, sodales sed metus id, sodales bibendum massa. Proin finibus nec nulla quis eleifend.
 
@@ -64,8 +69,9 @@ In massa nibh, rhoncus non pellentesque sit amet, fermentum sed turpis. Duis id 
 Donec ipsum diam, pharetra sit amet euismod in, condimentum vitae odio. Nunc non rhoncus neque. Curabitur ultricies, leo eget elementum maximus, ante justo vulputate enim, vitae ultricies leo magna non elit. Sed bibendum urna id orci semper, vel porta nunc posuere. Donec ligula magna, faucibus nec elementum et, molestie a odio. Mauris dapibus mollis justo, gravida condimentum sapien. Sed mi tortor, bibendum eu orci sit amet, eleifend eleifend purus. Etiam bibendum condimentum velit gravida malesuada. Donec semper lacus id risus rhoncus imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce malesuada convallis orci sit amet lacinia. Phasellus vel facilisis elit. Etiam scelerisque nisl ac feugiat suscipit. Nunc congue egestas nulla at venenatis. Proin ut sollicitudin urna. Nullam aliquet imperdiet cursus.
 
 Suspendisse nec erat sit amet sem posuere ornare. Proin neque tellus, placerat suscipit ipsum eget, scelerisque placerat velit. In eget pellentesque mi, nec gravida turpis. Aenean quis massa vitae eros accumsan gravida quis nec nisi. Donec vulputate laoreet diam. Ut quis rhoncus lacus. Sed egestas lobortis sagittis. Sed eget gravida libero. Sed sit amet tellus at nisi tempor scelerisque pellentesque vel odio. Donec dignissim, erat quis scelerisque posuere, ante elit porta erat, id convallis felis magna sed enim. Integer vitae porta leo. Nam ultrices quam ut neque ullamcorper, non semper metus sodales. In consequat sodales semper. Sed volutpat ligula in lorem mattis, volutpat malesuada sapien porttitor.
+`,
 
-
+`
 \`\`\`typescript
 import { Page } from "../../_types/PageShapes";
 import { Topics } from "../../_types/Topics";
