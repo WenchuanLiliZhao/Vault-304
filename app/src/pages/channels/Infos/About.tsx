892200074ac4956e378ee852cdc274132afeec58
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-12-02 15:11:07 +0800

    Update About.tsx

--- app/src/pages/channels/Infos/About.tsx
+++ app/src/pages/channels/Infos/About.tsx
@@ -1,16 +1,17 @@
-import { Page } from "../../_types/PageShapes"
+import { Page } from "../../_types/PageShapes";
 
 const About: Page = {
   info: {
     title: "About",
     path: "about",
-    summary: "Fusce dapibus massa vestibulum, imperdiet erat convallis, tristique lorem. Nunc lacinia enim urna. Vivamus condimentum risus eros, et auctor urna bibendum et. Aliquam erat volutpat. Vivamus mattis dictum nisl, quis accumsan nunc hendrerit id. Etiam in erat sed erat ornare bibendum."
+    summary:
+      "Fusce dapibus massa vestibulum, imperdiet erat convallis, tristique lorem. Nunc lacinia enim urna. Vivamus condimentum risus eros, et auctor urna bibendum et. Aliquam erat volutpat. Vivamus mattis dictum nisl, quis accumsan nunc hendrerit id. Etiam in erat sed erat ornare bibendum.",
   },
-  content: [(
-    <>
-      About
-    </>
-  )]
-}
+  content: [
+    `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam amet laudantium tenetur, similique ad, qui quisquam dicta harum voluptatibus, vero temporibus deleniti perferendis doloremque quam atque illo ex iste saepe?`,
+    `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam amet laudantium tenetur, similique ad, qui quisquam dicta harum voluptatibus, vero temporibus deleniti perferendis doloremque quam atque illo ex iste saepe?`,
+    `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam amet laudantium tenetur, similique ad, qui quisquam dicta harum voluptatibus, vero temporibus deleniti perferendis doloremque quam atque illo ex iste saepe?`,
+  ],
+};
 
-export default About
+export default About;
